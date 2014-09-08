50e5daf7c8f6ed06df0a7e9450bb40ac756bfeed
Vincent Bernier <gogluness@gmail.com>
2014-09-08 14:44:23 -0400

    Créer un group (WIP)
    ajout de view et de model

--- TrouveUnBand/Controllers/CreateGroupController.cs
+++ TrouveUnBand/Controllers/CreateGroupController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrouveUnBand.Models;
+using System.Collections;
 
 namespace TrouveUnBand.Controllers
 {
@@ -11,12 +13,19 @@ namespace TrouveUnBand.Controllers
         //
         // GET: /CreateGroup/
 
-        public ActionResult CreateGroup()
+        public ActionResult CreateGroup(GroupModel model)
         {
+            ArrayList al = new ArrayList();
+
+            //Boucle remplissant l'arraylist avec les style présent dans la table style
+
+            //boucle remplissant l'arraylist membre avec les donnée présent dans la table membre
+            
+
             return View();
         }
 
-        public ActionResult ConfirmCreateGroup()
+        public ActionResult ConfirmCreateGroup(GroupModel model)
         {
             return View();
         }
---
+++ TrouveUnBand/Models/GroupModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrouveUnBand.Models
+{
+    public class GroupModel
+    {
+        [Required]
+        string GroupName { get; set; }
+        [Required]
+        string[] Style { get; set; }
+        [Required]
+        string[] Member { get; set; }
+        [Required]
+        string Region { get; set; }
+        [Required]
+        string Experience { get; set; }
+
+    }
+}
