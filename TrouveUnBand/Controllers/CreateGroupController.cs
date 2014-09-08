1e488e166797954333b1d49608f2e82d0cd2d200
Vincent Bernier <gogluness@gmail.com>
2014-09-08 12:41:41 -0400

    Branch Create Group
    Permet a l'utilisateur de créer un group

---
+++ TrouveUnBand/Controllers/CreateGroupController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrouveUnBand.Controllers
+{
+    public class CreateGroupController : Controller
+    {
+        //
+        // GET: /CreateGroup/
+
+        public ActionResult CreateGroup()
+        {
+            return View();
+        }
+
+    }
+}
