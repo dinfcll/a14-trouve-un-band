3a588212f21705e532f7a82b7f73bc2085e6ad61
fredbegin11 <frederic.begin.fb@gmail.com>
2014-09-09 21:47:41 -0400

    Validation du formulaire avec Regex
    Ajout de plus de validation avec des expressions régulières

--- TrouveUnBand/Models/User.cs
+++ TrouveUnBand/Models/User.cs
@@ -10,15 +10,19 @@ namespace TrouveUnBand.Models
     public class User
     {
         [Required(ErrorMessage="This field is required")]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Must be at least 2 characters long, letters only")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Must be at least 2 characters long, letters only")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
+        [RegularExpression(@"^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$", ErrorMessage = "Must be a valid date")]
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
+        [RegularExpression(@"^[a-zA-Z0-9]{3,}$", ErrorMessage = "Must be at least 3 characters long")]
         public string Nickname { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
@@ -26,12 +30,15 @@ namespace TrouveUnBand.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Must be at least 2 letters long")]
         public string City { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
+        [RegularExpression(@"^[\S]{4,138}$", ErrorMessage = "Must be at least 4 letters long")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "This field is required")]
+        [RegularExpression(@"^[\S]{4,138}$", ErrorMessage = "Must be at least 4 letters long")]
         public string ConfirmPassword { get; set; }
     }
 }
