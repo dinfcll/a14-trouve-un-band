16faad4f136dca9699a84396cbf2b42603ce39d0
Vincent Bernier <gogluness@gmail.com>
2014-10-03 15:48:20 -0400

    Popup Comfirmation (WIP)
    La confirmation se fait dans un popup modal

--- cmd_insertions_tables.sql
+++ cmd_insertions_tables.sql
@@ -5,12 +5,85 @@ insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
 insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
 	values('Chuck','Norris',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
 
+insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user1','last1',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user2','last2',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user3','last3',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user4','last4',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user5','last5',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user6','last6',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user7','last7',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user8','last8',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user9','last9',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user10','last10',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user11','last11',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user12','last12',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
+	insert into Users(FirstName,LastName,BirthDate,Nickname,Email,Password,Location)
+	values('user13','last13',convert(datetime,'10/04/1952'),'Chucky','chucky@hotmail.com','12345','Lévis');
+	
 insert into Bands(Name, Description, Location)
 	values('The wild Mofos', 'We are the Wild Mofos and we fear nothing, even your mom', 'Las Vegas');
 
 insert into Bands(Name, Description, Location)
 	values('Loss Sanchez', 'Lost in the desert cooking pancakes!', 'Unknown');
-
+	
+insert into Bands(Name, Description, Location)
+	values('band1', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band2', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band3', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band4', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band5', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band6', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band7', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band8', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band9', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band10', 'test', 'Unknown');
+	
+	insert into Bands(Name, Description, Location)
+	values('band11', 'test', 'Unknown');
+	
+	
 insert into Musicians(UserId, Description)
   values(1, 'Good');
 
