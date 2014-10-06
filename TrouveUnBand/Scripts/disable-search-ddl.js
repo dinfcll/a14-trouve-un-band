64e354c9e0f12045d929fedaa16a1c2dc3cacb8f
Renaud Lainé <renlane@hotmail.com>
2014-10-06 14:49:28 -0400

    WIP Filtre

---
+++ TrouveUnBand/Scripts/disable-search-ddl.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    if ($('#DDLCategories option[value=4]')) {
+        $('#DDLGenres').prop('disabled', true);
+    } else {
+        $('#DDLGenres').prop('disabled', false);
+    }
+});
