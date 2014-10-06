6c6f0797777b913e4a820277626f2146a169490f
funny-vince <funny-vince@hotmail.com>
2014-10-05 17:04:48 -0700

    commit tooltips + rating

---
+++ TrouveUnBand/Scripts/Tooltips.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $('[data-toggle="tooltip"]').hover(function () {
+        $(this).tooltip('show');
+    }, function () {
+
+    });
+
+    $('[data-toggle="tooltip"]').hover(function () {
+
+    }, function () {
+        $(this).tooltip('destroy');
+    });
+});
