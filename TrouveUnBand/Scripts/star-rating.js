5541062a7dea2efdb0dcc020a2abe37a225a3ae3
funny-vince <funny-vince@hotmail.com>
2014-10-07 10:26:26 -0700

    commit input star-rating amilioration

--- TrouveUnBand/Scripts/star-rating.js
+++ TrouveUnBand/Scripts/star-rating.js
@@ -1,20 +1,40 @@
 ﻿$(document).ready(function () {
 
     var NbStars = 0;
+    var Rating = 0;
     var SkillsList = ["Débutant", "Initié", "Intermédiaire", "Avancé", "Légendaire"];
+    var Elements = document.querySelectorAll(".star-rating");
+
+    for (var i = 0; i < Elements.length; i++) {
+
+        Elements[i].setAttribute("data-animation", "false");
+        Elements[i].setAttribute("title", SkillsList[1]);
+        Elements[i].setAttribute("data-toggle", "tooltip");
+        NbStars = Elements[i].getAttribute("data-nb-stars");
+        Rating = Elements[i].getAttribute("data-rating");
+
+        for (var j = 0; j < NbStars; j++) {
+            if (j < Rating) {
+                Elements[i].innerHTML += '<i class="glyphicon glyphicon-star" data-prev-rating-class="glyphicon glyphicon-star"></i>';
+            }
+            else {
+                Elements[i].innerHTML += '<i class="glyphicon glyphicon-star-empty" data-prev-rating-class="glyphicon glyphicon-star-empty"></i>';
+            }
+        }
+    }
 
     $('.star-rating i').hover(function () {
-        NbStars=0;
+        Rating = 0;
         $(this).prevAll().removeClass('glyphicon-star-empty').addClass('glyphicon-star');
         $(this).removeClass('glyphicon-star-empty').addClass('glyphicon-star');
         $(this).nextAll().addClass('glyphicon-star-empty').removeClass('glyphicon-star');
 
         $(this).siblings('i').each(function () {
             if ($(this).hasClass('glyphicon-star')) {
-                NbStars = NbStars + 1;
+                Rating = Rating + 1;
             }
         });
-        $(this).parent('.star-rating').attr('data-original-title', SkillsList[NbStars]);
+        $(this).parent('.star-rating').attr('data-original-title', SkillsList[Rating]);
 
         $(this).parent('.star-rating').tooltip('hide')
         $(this).parent('.star-rating').tooltip('fixTitle')
@@ -26,27 +46,27 @@
     $('.star-rating').hover(function () {
         
     }, function () {
-        NbStars = 0;
+        Rating = 0;
         $(this).children('i').each(function () {
             $(this).removeClass('glyphicon-star-empty')
             $(this).attr('class', $(this).attr('data-prev-rating-class'));
             if ($(this).hasClass('glyphicon-star')) {
-                NbStars = NbStars + 1;
+                Rating = Rating + 1;
             }
         });
-        $(this).attr('data-original-title', SkillsList[NbStars-1]);
+        $(this).attr('data-original-title', SkillsList[Rating - 1]);
     });
 
     $('.star-rating').click(function () {
         
     }, function () {
-        NbStars=0;
+        Rating = 0;
         $(this).children('i').each(function () {
             $(this).attr('data-prev-rating-class', $(this).attr('class'));
             if ($(this).hasClass('glyphicon-star')) {
-                NbStars=NbStars+1;
+                Rating = Rating + 1;
             }
         });
-        $(this).attr('data-nb-stars', NbStars);
+        $(this).attr('data-rating', Rating);
     });
 });
