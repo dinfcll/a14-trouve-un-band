5ad305a91da82f831b12c2d32932171d5bbbc0de
funny-vince <funny-vince@hotmail.com>
2014-10-11 19:17:45 -0700

    commit gestion image

--- TrouveUnBand/Controllers/UsersController.cs
+++ TrouveUnBand/Controllers/UsersController.cs
@@ -24,6 +24,11 @@ namespace TrouveUnBand.Controllers
     {
         private TrouveUnBandEntities db = new TrouveUnBandEntities();
 
+        public ActionResult test()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -432,6 +437,29 @@ namespace TrouveUnBand.Controllers
         {
             return (Math.PI / 180) * val;
         }
+
+        //[HttpPost]
+        //public virtual ActionResult CropImage(string imagePath, int? cropPointX, int? cropPointY, int? imageCropWidth, int? imageCropHeight)
+        //{
+        //    byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+        //    byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+
+        //    string tempFolderName = Server.MapPath("~/" + ConfigurationManager.AppSettings["Image.TempFolderName"]);
+        //    string fileName = Path.GetFileName(imagePath);
+
+        //    try
+        //    {
+        //        FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //Log an error     
+        //        return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+        //    }
+
+        //    string photoPath = string.Concat("/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
+        //    return Json(new { photoPath = photoPath }, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
 
---
+++ TrouveUnBand/Scripts/ImageUpload.js
@@ -0,0 +1,85 @@
+﻿var imageCropWidth = 0;
+var imageCropHeight = 0;
+var cropPointX = 0;
+var cropPointY = 0;
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#PicToCrop').attr('src', e.target.result);
+            $('.jcrop-holder img').attr('src', e.target.result);
+            api.destroy();
+            initCrop();
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#ImageUploader").change(function () {
+    readURL(this);
+    $('#CropperDialog').modal('show');
+});
+
+
+$(document).ready(function () {
+    initCrop();
+});
+
+$("#hl-crop-image").on("click", function (e) {
+    e.preventDefault();
+    cropImage();
+});
+
+
+function initCrop() {
+    $('#PicToCrop').Jcrop({
+        onChange: setCoordsAndImgSize,
+        aspectRatio: 250 / 172,
+        addClass: 'jcrop-light',
+        bgOpacity: 0.4,
+        bgColor: 'black',
+    }, function () {
+        api = this;
+        api.setSelect([50, 50, 100, 250]);
+        api.setOptions({ bgFade: true });
+        api.ui.selection.addClass('jcrop-selection'); 
+    });
+}
+
+function setCoordsAndImgSize(e) {
+
+    imageCropWidth = e.w;
+    imageCropHeight = e.h;
+
+    cropPointX = e.x;
+    cropPointY = e.y;
+}
+
+function cropImage() {
+
+    if (imageCropWidth == 0 && imageCropHeight == 0) {
+        alert("Please select crop area.");
+        return;
+    }
+
+    $.ajax({
+        url: '/Image/CropImage',
+        type: 'POST',
+        data: {
+            imagePath: $("#my-origin-image").attr("src"),
+            cropPointX: cropPointX,
+            cropPointY: cropPointY,
+            imageCropWidth: imageCropWidth,
+            imageCropHeight: imageCropHeight
+        },
+        success: function (data) {
+            $("#my-cropped-image")
+                .attr("src", data.photoPath + "?t=" + new Date().getTime())
+                .show();
+        },
+        error: function (data) { }
+    });
+}
