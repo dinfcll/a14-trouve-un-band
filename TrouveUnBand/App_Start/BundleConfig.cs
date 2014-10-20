687d1f820e1f58105aab9e71a527a5ab0f00c921
Vincent Bernier <Gogluness@gmail.com>
2014-10-20 14:43:03 -0400

    Correction Bug Bundles
    Ajout du Bundles de Bootstrap dans Bundle Config qui était manquant
    empêchant les modals et les dropdown de fonctionner.

--- TrouveUnBand/App_Start/BundleConfig.cs
+++ TrouveUnBand/App_Start/BundleConfig.cs
@@ -8,8 +8,17 @@ namespace TrouveUnBand
         // Pour plus d’informations sur le Bundling, accédez à l’adresse http://go.microsoft.com/fwlink/?LinkId=254725 (en anglais)
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/bootstrap.min.css",
+                        "~/Content/bootstrap-responsive.min.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
