4ef97763f62de6dc9494c35ab3e398b270bf7e55
funny-vince <funny-vince@hotmail.com>
2014-10-23 22:27:33 -0700

    commit pour prochain cours, profil musicien info fini

---
+++ TrouveUnBand/Scripts/ProfilePageInfo.js
@@ -0,0 +1,19 @@
+﻿
+$(".profile-info-panel").hide();
+
+$(".profile-info-row").click(function () {
+    var chevron = $(this).children("i");
+    var panelId = $(this).attr("data-panel-id");
+    var panel = $("#" + panelId);
+
+    panel.slideToggle(400, function () {
+        if (panel.is(":visible")) {
+            chevron.removeClass("glyphicon-chevron-down");
+            chevron.addClass("glyphicon-chevron-up");
+        }
+        else {
+            chevron.removeClass("glyphicon-chevron-up");
+            chevron.addClass("glyphicon-chevron-down");
+        }
+    })
+});
