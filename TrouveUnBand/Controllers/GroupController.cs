6f04dc988cc4e74c8bbe2312705799fbb71beb18
Vincent Bernier <Gogluness@gmail.com>
2014-10-24 21:24:02 -0400

    Ajout Boostrap (WIP)
    implantation de bootstrap dans les page de groupe

--- TrouveUnBand/Controllers/GroupController.cs
+++ TrouveUnBand/Controllers/GroupController.cs
@@ -55,8 +55,9 @@ namespace TrouveUnBand.Controllers
                 bool b = CurrentUserIsMusician(CurrentUser, out CurrentMusician);
                 if (b)
                 {
-                    ViewData["myBand"] = new Band();
-                    ViewData["Musicians"] = CurrentMusician;
+                    Band myBand = new Band();
+                    myBand.Musicians = CurrentMusician;
+                    ViewData["myBand"] = myBand;
                     //à la création de la vue le premier musicien est toujours le musicien associé au compte authentifié.
                     ViewBag.CurrentMusician = CurrentMusician[0];
                     ViewBag.GenrelistDD = new List<Genre>(db.Genres);
