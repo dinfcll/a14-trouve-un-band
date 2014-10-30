9798d86bea7c5122671fbd03ce4837922f8d2446
Renaud <renlane@hotmail.com>
2014-10-29 21:24:27 -0400

    Divison des fitlres du search view en partial views

---
+++ TrouveUnBand/Scripts/search-page.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $('#advanced-filter').hide();
+
+    /**
+    * This function hides the 'Genres' dropdown list when 'Utilisateur' is selected
+    * in the 'Categorie' dropdown list.
+    */
+    $('#ddlCategories').change(function () {
+        var selectedItem = $('#ddlCategories').val()
+        if (selectedItem == 'option_user') {
+            $('#ddlGenres').hide();
+            $('label[for="ddlGenres"]').hide();
+        } else {
+            $('#ddlGenres').show();
+            $('label[for="ddlGenres"]').show();
+        }
+    });
+
+    /**
+    * This function show/hide the advanced search section.
+    */
+    $('#toggle-filter').click(function () {
+        var text = $(this).text();
+        if (text == 'Filtre avancée') {
+            $(this).text('Filtre simple');
+        }
+        else {
+            $(this).text('Filtre avancée');
+        }
+        $('#basic-filter').toggle();
+        $('#advanced-filter').toggle();
+    });
+
+    $('li.searchfilter').on('click', function () {
+        var genre = $(this).children('span').text();
+        var label = '<li id="tags" class="label label-default">' + genre + '<span class="glyphicon-remove"></span></li>';
+        $('#search-parameters').append(label);
+    });
+
+    $('#search-parameters').on('click', function () {
+        alert("yo");
+    });
+});
