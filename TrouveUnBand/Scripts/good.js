2c86db498fd3c3745045ce12b35a616d7965caf2
Vincent Bernier <Gogluness@gmail.com>
2014-11-21 14:38:11 -0500

    test SDK soundcloud
    affiche un Embed de 49 chansons au hasard d'apres le genre sélectionné
    sur une page test

---
+++ TrouveUnBand/Scripts/good.js
@@ -0,0 +1,37 @@
+﻿function PlaySomeSound(genre) {
+    console.log("bonjour");
+    SC.get('/tracks',
+    {
+        genres: genre,
+        bpm:
+        {
+            from: 100
+        }
+    }, function(tracks) {
+        var random = Math.floor(Math.random() * 49);
+        console.log(random);
+        console.log(tracks[random]);
+        console.log(tracks[random].uri);
+        SC.oEmbed(tracks[random].uri, {
+            auto_play: true
+        }, document.getElementById('target'));
+    });
+}
+
+window.onload = function() {
+    SC.initialize({
+        client_id: '96fcd16e1df481197be64f94a6d0f35a'
+    });
+
+    var menuLinks = document.getElementsByClassName('genre');
+
+    for (var i = 0; i < menuLinks.length; i++) {
+        console.log(menuLinks[i]);
+        var menuLink = menuLinks[i];
+        menuLinks[i].onclick = function(e) {
+            e.preventDefault();
+            PlaySomeSound(menuLink.innerHTML);
+            console.log(menuLink.innerHTML);
+        };
+    }
+};
