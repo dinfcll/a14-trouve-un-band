c1b94c6a8bad64a869813267eafb1e567ecd5ea8
fredbegin11 <frederic.begin.fb@gmail.com>
2014-11-24 13:43:27 -0500

    Commit Vue Advert

--- TrouveUnBand/Models/Partial/_Event.cs
+++ TrouveUnBand/Models/Partial/_Event.cs
@@ -33,7 +33,7 @@ namespace TrouveUnBand.Models
             public float Salary { get; set; }
             public List<Genre> Genres { get; set; }
             [Required(ErrorMessage = "Ce champ est requis")]
-            //[RegularExpression(@"^[0-9]{1,}$", ErrorMessage = "Doit être composé de chiffres seulement")]
+            [RegularExpression(@"^[PMLpml]{1}$", ErrorMessage = "Doit être P, M ou L")]
             public string StageSize { get; set; }
             public byte[] Photo { get; set; }
             public string Creator_ID { get; set; }
