b90b8ed481cd08bfb16eec9b295f8e7dcf6d7085
Vincent Bernier <Gogluness@gmail.com>
2014-11-29 11:36:15 -0500

    Validation duplicat

--- TrouveUnBand/Scripts/AddBandMember.js
+++ TrouveUnBand/Scripts/AddBandMember.js
@@ -2,11 +2,15 @@
 var TrId = 0;
 
 function AddBandMember(json) {
-    bandMembers.push(json);
-    var div = document.getElementById('band-members');
-    $('#band-members').append('<input id="'+ json["User_ID"] +'" type="button" value="' + json["FirstName"] + '" onclick="RemoveBandMember(' + json["User_ID"] + ')" />');
-    TrId++;
-    alert(bandMembers);
+    var testResult = $.grep(bandMembers, function(value) {
+        return value != json;
+    });
+    if (testResult.length === 0) 
+    {
+        bandMembers.push(json);
+        var div = document.getElementById('band-members');
+        $('#band-members').append('<input id="' + json["User_ID"] + '" type="button" value="' + json["FirstName"] + '" onclick="RemoveBandMember(' + json["User_ID"] + ')" />');
+    }
 };
 
 function RemoveBandMember(i) {
