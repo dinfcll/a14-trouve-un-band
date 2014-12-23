41e8d722ec60f3cc2a2a89990b91e3dd6c7dd91d
funny-vince <funny-vince@hotmail.com>
2014-12-23 14:25:20 -0800

    commit nouvelle fonction tbody calendar

--- TrouveUnBand/Scripts/calendar.js
+++ TrouveUnBand/Scripts/calendar.js
@@ -10,7 +10,11 @@ $.fn.buildCalendar = function (options) {
 
 function createCalendarHead() {
     var currentMonth = monthNames[moment().month()];
-    var tHead = '<div id="calender-head">' + currentMonth + ', ' +moment().year() + '</div>';
+    var tHead = '<div id="calender-head">'
+                + '<span class="glyphicon glyphicon-chevron-left"></span>'
+                + '<span>' + currentMonth + ', ' + moment().year() + '</span>'
+                + '<span class="glyphicon glyphicon-chevron-right"></span>'
+                + '</div>';
 
     return tHead;
 }
@@ -19,17 +23,23 @@ function createCalendarBody() {
     var tBody = '<tbody>' +
                 '<tr id="first-row"><td>DIM</td><td>LUN</td><td>MAR</td><td>MER</td><td>JEU</td><td>VEN</td><td>SAM</td></td>';
 
-    var firstDayOfMonth = moment().date(1).day();
-    var debuteDate = moment().date(1).subtract(firstDayOfMonth, 'days');
+    var debuteDate = moment().date(1);
+    var isSameMonth = moment().isSame(debuteDate, 'month');
+    var i = 0;
 
-    for (i = 0; i < 6; i++)
-    {
+    while (isSameMonth === true) {
+
         tBody += '<tr>';
-        for (j = 0; j < 7; j++) {
+
+        while (i < 7 && isSameMonth === true) {
             tBody += '<td>' + debuteDate.date() + '</td>';
             debuteDate.add(1, 'days');
+            isSameMonth = moment().isSame(debuteDate, 'month');
+            i++;
         }
+
         tBody += '</tr>';
+        i = 0;
     }
 
     tBody += '</tbody></table>';
