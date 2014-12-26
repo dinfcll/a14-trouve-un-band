9c47c5490d3ad2c7f8df4f88ef7859033d65ce9b
Vincent Bernier <Gogluness@gmail.com>
2014-12-26 13:11:01 -0500

    ChatHub tutorial (WIP)
    http://www.asp.net/signalr/overview/older-versions/tutorial-getting-started-with-signalr-and-mvc-4

---
+++ TrouveUnBand/Controllers/NotificationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrouveUnBand.Controllers
+{
+    public class NotificationController : Controller
+    {
+        //
+        // GET: /Notification/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Notification()
+        {
+            return View();
+        }
+
+    }
+}
--- TrouveUnBand/Global.asax.cs
+++ TrouveUnBand/Global.asax.cs
@@ -16,6 +16,7 @@ namespace TrouveUnBand
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapHubs();
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
