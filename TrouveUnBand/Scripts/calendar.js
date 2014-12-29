970162a5f307458e16f648134870d6be88b434b1
funny-vince <funny-vince@hotmail.com>
2014-12-29 11:00:48 -0800

    commit changement de mois sans css

--- TrouveUnBand/Scripts/calendar.js
+++ TrouveUnBand/Scripts/calendar.js
@@ -1,96 +1,104 @@
-﻿var monthNames = ["JANVIER", "FÉVRIER", "MARS", "AVRIL", "MAI", "JUIN",
-    "JUILLET", "AOÛT", "SEPTEMBRE", "OCTOBRE", "NOVEMBRE", "DÉCEMBRE"];
+﻿(function ($) {
+    var monthNames = ["JANVIER", "FÉVRIER", "MARS", "AVRIL", "MAI", "JUIN",
+            "JUILLET", "AOÛT", "SEPTEMBRE", "OCTOBRE", "NOVEMBRE", "DÉCEMBRE"];
 
-$.fn.buildCalendar = function (options) {
-    var args = Array.prototype.slice.call(arguments);
-    setArguments(args);
-    var calendar = '<table class="col-md-12">';
-    calendar += createCalendarHead();
-    calendar += createCalendarBody();
-    this.html(calendar);
-    SetTodayClass();
-}
+    function createCalendarHead(calendarMonth) {
 
-function setArguments(args) {
-    //var args2 = Array.prototype.slice.call(args, 1);
-    //alert(args2);
-    //for (var i = 0; i < args2.length; i++) {
-    //    alert("lol");
-    //}
-    alert(args.length);
-}
+        var month = monthNames[moment(calendarMonth).month()];
+        var year = moment(calendarMonth).year();
 
-function createCalendarHead() {
-    var currentMonth = monthNames[moment().month()];
-    var tHead = '<div id="calendar-head">'
-                + '<span class="glyphicon glyphicon-chevron-left"></span>'
-                + '<span id="calendar-title">' + currentMonth + ', ' + moment().year() + '</span>'
-                + '<span class="glyphicon glyphicon-chevron-right"></span>'
-                + '</div>';
+        var tHead = '<div id="calendar-head" class="col-md-12">'
+                    + '<span class="glyphicon glyphicon-chevron-left col-md-1"></span>'
+                    + '<span id="calendar-title" class="col-md-5">' + month + ', ' + year + '</span>'
+                    + '<span class="glyphicon glyphicon-chevron-right col-md-1"></span>'
+                    + '</div>';
 
-    return tHead;
-}
+        return tHead;
+    }
 
-function createCalendarBody() {
-    var tBody = '<tbody>' +
-                '<tr id="first-row"><td>DIM</td><td>LUN</td><td>MAR</td><td>MER</td><td>JEU</td><td>VEN</td><td>SAM</td></td>';
+    function createCalendarBody(calendarMonth) {
+        var tBody = '<tbody>' +
+                    '<tr id="first-row"><td>DIM</td><td>LUN</td><td>MAR</td><td>MER</td><td>JEU</td><td>VEN</td><td>SAM</td></td>';
 
-    var debuteDate = moment().date(1);
-    var isSameMonth = moment().isSame(debuteDate, 'month');
-    var i = 0;
+        var debuteDate = moment(calendarMonth).date(1);
+        var isSameMonth = moment(calendarMonth).isSame(debuteDate, 'month');
+        var i = 0;
 
-    while (isSameMonth === true) {
+        while (isSameMonth === true) {
 
-        tBody += '<tr>';
+            tBody += '<tr>';
 
-        while (i < 7 && isSameMonth === true) {
-            tBody += '<td>' + debuteDate.date() + '</td>';
-            debuteDate.add(1, 'days');
-            isSameMonth = moment().isSame(debuteDate, 'month');
-            i++;
+            while (i < 7 && isSameMonth === true) {
+                tBody += '<td>' + debuteDate.date() + '</td>';
+                debuteDate.add(1, 'days');
+                isSameMonth = moment(calendarMonth).isSame(debuteDate, 'month');
+                i++;
+            }
+
+            tBody += '</tr>';
+            i = 0;
         }
 
-        tBody += '</tr>';
-        i = 0;
+        tBody += '</tbody></table>';
+
+        return tBody;
     }
 
-    tBody += '</tbody></table>';
+    function setTodayClass(calendarMonth) {
+        if (moment().isSame(calendarMonth, 'month')) {
+            $("tr td:contains('" + moment().date() + "')").addClass("today");
+        }
+    }
 
-    return tBody;
-}
+    function changeMonth(nextMonthPosition) {
+        var calendarTitle = $("#calendar-title").text().split(",");
 
-function SetTodayClass() {
-    $("tr td:contains('" + moment().date() + "')").addClass("today");
-}
+        var currentCalendarMonth = $.trim(calendarTitle[0]);
+        currentCalendarMonth = monthNames.indexOf(currentCalendarMonth);
 
-function ChangeMonth(nextMonthPosition) {
-    var calendarTitle = $("#calendar-title").text().split(",");
+        var currentCalendarYear = $.trim(calendarTitle[1]);
 
-    var currentCalendarMonth = $.trim(calendarTitle[0]);
-    currentCalendarMonth = monthNames.indexOf(currentCalendarMonth);
+        $("#calendar").buildCalendar({
+            month: currentCalendarMonth,
+            year: currentCalendarYear,
+            changeMonthBy: nextMonthPosition
+        });
+    }
 
-    var currentCalendarYear = $.trim(calendarTitle[1]);
-
-    $("#calendar").buildCalendar({
-        month: currentCalendarMonth,
-        year: currentCalendarYear,
-        changeMonthBy: nextMonthPosition
+    $("#calendar").on("click", "tr:not(#first-row) td", function () {
+        $("#calendar td").removeClass("active");
+        $(this).addClass("active");
     });
-}
 
-$("#calendar").on("click", "tr:not(#first-row) td", function () {
-    $("#calendar td").removeClass("active");
-    $(this).addClass("active");
-});
+    $("#calendar").on("click", "#calendar-head .glyphicon", function () {
+        if ($(this).hasClass("glyphicon-chevron-left")) {
+            changeMonth(-1);
+        } else {
+            changeMonth(1);
+        }
+    });
 
-$("#calendar").on("click", "#calendar-head .glyphicon", function () {
-    if ($(this).hasClass("glyphicon-chevron-left")) {
-        ChangeMonth(-1);
-    } else {
-        ChangeMonth(1);
-    }
-});
+    $.fn.buildCalendar = function (options) {
 
+        var defaults =
+        {
+            month: moment().month(),
+            year: moment().year(),
+            changeMonthBy: 0
+        }
 
-//faire en sorte que le calendrier se load avec [data-type=calendar] ou quelque chose dans le genre
-//je suis rendu a recevoir le mois en paramètre 
+        var parameters = $.extend(defaults, options);
+
+        var calendarMonth = moment()
+            .set('month', parameters.month)
+            .set('year', parameters.year)
+            .add(parameters.changeMonthBy, 'months');
+
+        var calendar = '<table class="col-md-12">';
+        calendar += createCalendarHead(calendarMonth);
+        calendar += createCalendarBody(calendarMonth);
+        this.html(calendar);
+        setTodayClass(calendarMonth);
+    };
+
+})(jQuery);
